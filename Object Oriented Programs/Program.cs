9f38e52c629c5be8fda237526401bd76aea5f646
ANMOL <anmol999rajput@gmail.com>
2022-03-20 14:18:32 +0530

    Add project files.

---
+++ Object Oriented Programs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Object_Oriented_Programs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
